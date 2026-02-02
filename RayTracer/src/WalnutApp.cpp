c32463bdc108cdaa0526c36977a294789518ad4c
man121212 <130829508+WhistleBlower264@users.noreply.github.com>
2026-02-02 23:40:28 +0530

    ray casted a mint-colored sphere

--- RayTracer/src/WalnutApp.cpp
+++ RayTracer/src/WalnutApp.cpp
@@ -2,9 +2,9 @@
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
-#include "Walnut/Random.h"
 #include "Walnut/Timer.h"
-
+    
+#include "C:\Users\kokan\OneDrive\Desktop\Projects\RayTracer-with-vulkan\RayTracer\src\Renderer.h"
 
 using namespace Walnut;
 using namespace std;
@@ -22,14 +22,16 @@ public:
 
 		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
 		ImGui::Begin("Viewport");
+
 		ImGui::Text("LastRender: %.3fms", m_LastRenderTime);
 
 		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
 		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
-		
-		if (m_Image)
+
+		auto image = m_Renderer.GetFinalImage();
+		if (image)
 		{
-			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
+			ImGui::Image(image->GetDescriptorSet(), {(float)image->GetWidth(),(float)image->GetHeight() }, ImVec2(0,1),ImVec2(1,0));
 		}
 		ImGui::End();
 		ImGui::PopStyleVar();
@@ -40,27 +42,16 @@ public:
 	{
 		Timer timer;
 
-		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight())
-		{
-			m_Image = make_shared<Image>(m_ViewportWidth ,m_ViewportHeight, ImageFormat::RGBA);
-			delete[] m_ImageData;
-			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
-		}
-		for(uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++)
-		{
-			m_ImageData[i] = Random::UInt();
-			m_ImageData[i] |= 0xff000000;
-		}
-		m_Image->SetData(m_ImageData);
-		m_LastRenderTime = timer.ElapsedMillis();
+		m_Renderer.OnResize(m_ViewportWidth, m_ViewportHeight);
+		m_Renderer.Render();
+
+		m_LastRenderTime = timer.ElapsedMillis();   
 	}
 
-private :
-	shared_ptr<Image> m_Image;	
-	uint32_t* m_ImageData = nullptr;
+private:
+	Renderer m_Renderer;
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 	float m_LastRenderTime = 0.0f;
-
 };
 
 
